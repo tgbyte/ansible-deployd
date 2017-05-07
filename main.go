36d6459d9eebf214e9ea7f8db21b7a77a5e1e062
Stefan Reuter <stefan.reuter@reucon.com>
2017-05-07 12:42:24 +0200

    Run git pull and ansible-playbook

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/caarlos0/env"
 	"github.com/gorilla/mux"
-	"net/http"
-	"log"
 )
 
 type Config struct {
-	Limit     []string      `env:"LIMIT" envSeparator:","`
-	Playbooks []string      `env:"PLAYBOOKS" envSeparator:","`
+	Limit     []string `env:"LIMIT" envSeparator:","`
+	Playbooks []string `env:"PLAYBOOKS" envSeparator:","`
+	WorkDir   string   `env:"WORK_DIR" envDefault:"/ansible"`
 }
 
 var config Config
@@ -24,10 +26,11 @@ func main() {
 func run() int {
 	err := env.Parse(&config)
 	check(err)
-	fmt.Printf("%+v\n", config)
+	log.Printf("Configuration: %+v", config)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/deploy/{limit:[a-zA-Z0-9_-]+}/{playbook:[a-zA-Z0-9_-]+}", DeployHandler)
+	r.HandleFunc("/deploy/{limit:[a-zA-Z0-9_-]+}/{playbook:[a-zA-Z0-9_-]+}", DeployHandler).
+		Methods(http.MethodPost)
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 
@@ -49,8 +52,47 @@ func DeployHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	out, err := gitPull()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Git exited with error: %s\n\n%s", err, out)
+		return
+	}
+
+	out, err = runAnsible(limit, playbook)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Ansible exited with error: %s\n\n%s", err, out)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Limit: %v, Playbook: %v\n", limit, playbook)
+	fmt.Fprintf(w, "%s", out)
+
+}
+
+func gitPull() (string, error) {
+	log.Print("Running git pull")
+	return runCommand("git", "pull")
+}
+
+func runAnsible(limit string, playbook string) (string, error) {
+	log.Printf("Deploying %s to %s", playbook, limit)
+	return runCommand("ansible-playbook", "-e", "docker_container_state=deploy", "--limit", limit, "playbooks/"+playbook+".yml")
+}
+
+func runCommand(name string, arg ...string) (string, error) {
+	cmd := exec.Command(name, arg...)
+	cmd.Dir = config.WorkDir
+	out, err := cmd.CombinedOutput()
+
+	log.Printf("%s output: %q", name, string(out))
+	if err != nil {
+		log.Printf("%s exited with error: %s", name, err)
+		return string(out), err
+	}
+
+	return string(out), nil
 }
 
 func contains(s []string, e string) bool {
@@ -67,4 +109,3 @@ func check(e error) {
 		panic(e)
 	}
 }
-
